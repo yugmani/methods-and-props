0b777542dbb06896f8393f36ac5af74f9354ff90
yugmani <yugmani@yahoo.com>
2021-06-05 12:51:12 -0700

    source mention

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ export default function App() {
     <div className="app">
       <h1>-----Method as Props-----</h1>
       <ParentComponent />
+      <div className="footer">
+        <a
+          href="https://www.geeksforgeeks.org/reactjs-methods-as-props/"
+          target="_blank"
+        >
+          Source: ReactJS | Methods as Props{' '}
+        </a>
+      </div>
     </div>
   );
 }
